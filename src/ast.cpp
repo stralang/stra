888538bf9f06a272893657cd0ea929f9b3ec07a1
PixelDoted <isaac@marketplaceplus.com>
2026-05-11 13:38:03 -0500

    Parse binary expression

--- src/ast.cpp
+++ src/ast.cpp
@@ -3,10 +3,42 @@
 #include "token.hpp"
 #include <cassert>
 
-#define try(is_eof)                                                            \
-  if (is_eof) {                                                                \
+#define try(is_ok)                                                             \
+  if (!(is_ok)) {                                                              \
     return nullptr;                                                            \
   }
+
+// Forward Declaration
+Node *parseExpr(ASTParser *parser, Precedence min_precedence);
+
+Node *parseBinaryExpr(ASTParser *parser, Node *atom,
+                      Precedence min_precedence) {
+  Node *out = atom;
+
+  while (parser->cur_token.kind == TokenKind::Operator) {
+    Operator opcode = parser->cur_token._operator;
+    Precedence precedence = operatorPrecedence(opcode);
+    if (precedence < min_precedence) {
+      break;
+    }
+
+    Associativity associativity = operatorAssociativity(opcode);
+    precedence = (Precedence)((int32_t)precedence + (int32_t)associativity);
+
+    out = (Node *)parser->allocator.alloc(sizeof(Node));
+    out->kind = NodeKind::Operator;
+    out->token = parser->cur_token;
+    out->location = out->token.location;
+    out->_operator.opcode = opcode;
+    out->_operator.lhs = atom;
+
+    try(parser->nextToken());
+    out->_operator.rhs = parseExpr(parser, precedence);
+    try(out->_operator.rhs != nullptr);
+  }
+
+  return out;
+}
 
 Node *parseExpr(ASTParser *parser, Precedence min_precedence) {
   Node *out = (Node *)parser->allocator.alloc(sizeof(Node));
@@ -21,12 +53,22 @@ Node *parseExpr(ASTParser *parser, Precedence min_precedence) {
 
     try(parser->nextToken());
     out->unary_operator.child = parseExpr(parser, Precedence::MemberAccess);
+    try(out->unary_operator.child != nullptr);
     break;
   }
   case TokenKind::Name: {
     out->kind = NodeKind::Name;
     out->text = parser->cur_token.text;
     parser->nextToken();
+    break;
+  }
+  }
+
+  // Parse Operator
+  switch (parser->cur_token.kind) {
+  case TokenKind::Operator: {
+    out = parseBinaryExpr(parser, out, min_precedence);
+    try(out != nullptr);
     break;
   }
   }
