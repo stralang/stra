416db9d203213444dba88b036866649999ccdb39
PixelDoted <isaac@marketplaceplus.com>
2026-05-19 09:48:25 -0500

    Evaluate Unary/Binary Operators

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -15,9 +15,308 @@
     return;                                                                    \
   }
 
+// Forward Declarations
+void evaluate(Evaluator *evaluator, Node *node, Scope *scope);
+
+bool compareTypes(Type *lhs, Type *rhs) {
+  if (lhs->kind != rhs->kind) {
+    return false;
+  }
+
+  switch (lhs->kind) {
+  case TypeKind::Void: {
+    return true;
+  }
+  case TypeKind::Bool: {
+    return true;
+  }
+  case TypeKind::Integer: {
+    return lhs->integer.is_signed == rhs->integer.is_signed &&
+           (lhs->integer.is_untyped || rhs->integer.is_untyped ||
+            lhs->integer.bits == rhs->integer.bits);
+  }
+  case TypeKind::Float: {
+    return lhs->_float.is_untyped || rhs->_float.is_untyped ||
+           lhs->_float.bits == rhs->_float.bits;
+  }
+  case TypeKind::Pointer: {
+    return compareTypes(lhs->child, rhs->child);
+  }
+  case TypeKind::Slice:
+  case TypeKind::SIMD: {
+    return lhs->slice.length == rhs->slice.length &&
+           compareTypes(lhs->slice.type, rhs->slice.type);
+  }
+  case TypeKind::TypeId: {
+    return true;
+  }
+  case TypeKind::Function: {
+    if (lhs->function.arguments.length != rhs->function.arguments.length) {
+      return false;
+    }
+
+    for (size_t i = 0; i < lhs->function.arguments.length; i++) {
+      if (!compareTypes(lhs->function.arguments.data.ptr[i],
+                        rhs->function.arguments.data.ptr[i])) {
+        return false;
+      }
+    }
+
+    return compareTypes(lhs->function.return_type, rhs->function.return_type);
+  }
+  case TypeKind::Struct: {
+    for (size_t i = 0; i < lhs->_struct.fields.length; i++) {
+      if (!compareTypes(lhs->_struct.fields.data.ptr[i],
+                        rhs->_struct.fields.data.ptr[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  case TypeKind::Enum: {
+    return compareTypes(lhs->_enum.repr_type, rhs->_enum.repr_type);
+  }
+  case TypeKind::Union: {
+    for (size_t i = 0; i < lhs->_union.variants.length; i++) {
+      if (!compareTypes(lhs->_union.variants.data.ptr[i],
+                        rhs->_union.variants.data.ptr[i])) {
+        return false;
+      }
+    }
+    return compareTypes(lhs->_union.repr_type, rhs->_union.repr_type);
+  }
+  }
+
+  return false;
+}
+
 Value execute(Evaluator *evaluator, Node *node, Scope *scope) {
   std::cerr << "TODO: Execute compile-time\n";
   return Value{.type = nullptr};
+}
+
+void evaluateUnary(Evaluator *evaluator, Node *node, Scope *scope) {
+  evaluate(evaluator, node->unary_operator.child, scope);
+
+  Type *child_type = node->unary_operator.child->value.type;
+  Type *child_primitive = child_type;
+  if (child_primitive->kind == TypeKind::SIMD) {
+    child_primitive = child_primitive->child;
+  }
+
+  switch (node->unary_operator.opcode) {
+  case UnaryOperator::Minus: {
+    if (child_primitive->kind != TypeKind::Integer &&
+        child_primitive->kind != TypeKind::Float) {
+      expect(false, node->unary_operator.child->location,
+             "Child must be of Integer, Float, or SIMD. Got `" << *child_type
+                                                               << "`");
+    }
+
+    Type out_type = *child_type;
+    if (child_primitive->kind == TypeKind::Integer &&
+        !child_primitive->integer.is_signed) {
+      if (out_type.kind == TypeKind::SIMD) {
+        Type prim_out_type = *out_type.child;
+        prim_out_type.integer.is_signed = true;
+        out_type.child = evaluator->type_cache->get(prim_out_type);
+      } else {
+        out_type.integer.is_signed = true;
+      }
+    }
+
+    out_type.is_constant = true;
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case UnaryOperator::Logical_Not: {
+    expect(child_primitive->kind == TypeKind::Bool,
+           node->unary_operator.child->location,
+           "Child must be Bool, or SIMD. Got `" << *child_type << "`");
+
+    Type out_type = *child_type;
+    out_type.is_constant = true;
+
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case UnaryOperator::Bitwise_Not: {
+    expect(child_primitive->kind == TypeKind::Integer,
+           node->unary_operator.child->location,
+           "Child must be Integer, or SIMD. Got `" << *child_type << "`");
+
+    Type out_type = *child_type;
+    out_type.is_constant = true;
+
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case UnaryOperator::Reference: {
+    Type out_type = {
+        .kind = TypeKind::Pointer, .child = child_type, .is_constant = true};
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case UnaryOperator::Dereference: {
+    expect(child_type->kind == TypeKind::Pointer,
+           node->unary_operator.child->location,
+           "Child must be Pointer. Got `" << *child_type << "`");
+
+    node->value.type = child_type->child;
+    node->value.has_data = false;
+    break;
+  }
+  }
+}
+
+void evaluateBinary(Evaluator *evaluator, Node *node, Scope *scope) {
+  evaluate(evaluator, node->_operator.lhs, scope);
+  Node *lhs = node->_operator.lhs;
+  if (node->_operator.opcode == Operator::MemberAccess) {
+    // TODO: Member Access
+    return;
+  }
+
+  evaluate(evaluator, node->_operator.rhs, scope);
+  Node *rhs = node->_operator.rhs;
+
+  // Assign
+  if (node->_operator.opcode == Operator::Assign) {
+    expect(!lhs->value.type->is_constant, lhs->location,
+           "Cannot assign to constant");
+
+    expect(compareTypes(lhs->value.type, rhs->value.type), rhs->location,
+           "cannot assign RHS `" << *rhs->value.type << "` to LHS `"
+                                 << *lhs->value.type << "`");
+
+    // TODO: Check Types
+    node->value.type = evaluator->type_cache->get({.kind = TypeKind::Void});
+    node->value.has_data = false;
+    return;
+  }
+
+  // Get primitive type
+  Type *lhs_primitive = lhs->value.type;
+  Type *rhs_primitive = rhs->value.type;
+  if (lhs_primitive->kind == TypeKind::SIMD) {
+    lhs_primitive = lhs_primitive->child;
+  }
+  if (rhs_primitive->kind == TypeKind::SIMD) {
+    rhs_primitive = rhs_primitive->child;
+  }
+
+  switch (node->_operator.opcode) {
+  case Operator::Add:
+  case Operator::Sub:
+  case Operator::Mul:
+  case Operator::Div:
+  case Operator::Mod: {
+    if (lhs_primitive->kind != TypeKind::Integer &&
+        lhs_primitive->kind != TypeKind::Float &&
+        lhs_primitive->kind != TypeKind::Pointer) {
+      expect(false, lhs->location,
+             "LHS must be of Integer, Float, Pointer, or SIMD. Got `"
+                 << *lhs->value.type << "`");
+    }
+
+    expect(compareTypes(lhs_primitive, rhs_primitive), rhs->location,
+           "LHS `" << *lhs->value.type << "` cannot operate with RHS `"
+                   << *rhs->value.type);
+
+    Type out_type = *lhs->value.type;
+    out_type.is_constant = true;
+
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::Bitwise_Or:
+  case Operator::Bitwise_Xor:
+  case Operator::Bitwise_And:
+  case Operator::Bitwise_LeftShift:
+  case Operator::Bitwise_RightShift: {
+    expect(lhs_primitive->kind == TypeKind::Integer, lhs->location,
+           "LHS must be an Integer. Got `" << *lhs->value.type << "`");
+    expect(compareTypes(lhs_primitive, rhs_primitive), rhs->location,
+           "LHS `" << *lhs->value.type << "` cannot operate with RHS `"
+                   << *rhs->value.type << "`\n");
+
+    Type out_type = *lhs->value.type;
+    out_type.is_constant = true;
+
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::Logical_Or:
+  case Operator::Logical_And: {
+    expect(lhs_primitive->kind == TypeKind::Bool, lhs->location,
+           "LHS must be a Bool. Got `" << *lhs->value.type << "`");
+    expect(compareTypes(lhs_primitive, rhs_primitive), rhs->location,
+           "LHS `" << *lhs->value.type << "` cannot operate with RHS `"
+                   << *rhs->value.type << "`\n");
+
+    Type out_type = *lhs->value.type;
+    out_type.is_constant = true;
+
+    node->value.type = evaluator->type_cache->get(out_type);
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::EqualTo:
+  case Operator::NotEqualTo: {
+    expect(lhs->value.type == rhs->value.type, rhs->location,
+           "LHS `" << *lhs->value.type << "` cannot operate with RHS `"
+                   << *rhs->value.type << "`");
+
+    node->value.type = evaluator->type_cache->get(
+        {.kind = TypeKind::Bool, .is_constant = true});
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::LessThen:
+  case Operator::GreaterThen:
+  case Operator::LessThenOrEqualTo:
+  case Operator::GreaterThenOrEqualTo: {
+    if (lhs_primitive->kind != TypeKind::Integer &&
+        lhs_primitive->kind != TypeKind::Float) {
+      expect(false, lhs->location,
+             "LHS must be of Integer, Float, or SIMD. Got `" << *lhs->value.type
+                                                             << "`");
+    }
+
+    expect(compareTypes(lhs_primitive, rhs_primitive), rhs->location,
+           "LHS `" << *lhs->value.type << "` cannot operate with RHS `"
+                   << *rhs->value.type << "`");
+
+    node->value.type = evaluator->type_cache->get(
+        {.kind = TypeKind::Bool, .is_constant = true});
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::As:
+  case Operator::Bitcast: {
+    expect(lhs->value.type->kind != TypeKind::TypeId, lhs->location,
+           "LHS must not be a type");
+    expect(rhs->value.type->kind == TypeKind::TypeId, rhs->location,
+           "RHS must be a type");
+
+    node->value.type = rhs->value.data.type_value;
+    node->value.has_data = false;
+    break;
+  }
+  case Operator::Assign:
+  case Operator::MemberAccess:
+  case Operator::Unary_Logical_Not:
+  case Operator::Unary_Bitwise_Not: {
+    // Already handled
+    break;
+  }
+  }
 }
 
 void evaluate(Evaluator *evaluator, Node *node, Scope *scope) {
@@ -109,7 +408,13 @@ void evaluate(Evaluator *evaluator, Node *node, Scope *scope) {
       expect(value->type != nullptr, node->field.initial->location,
              "Failed to evaluate field initial");
       if (node->value.type == nullptr) {
-        node->value.type = value->type;
+        if (value->type->is_constant && !node->field.definition) {
+          Type field_type = *value->type;
+          field_type.is_constant = false;
+          node->value.type = evaluator->type_cache->get(field_type);
+        } else {
+          node->value.type = value->type;
+        }
       } else if (node->value.type != value->type) {
         std::cerr << node->field.initial->location
                   << " Field initial doesn't match type.\n";
@@ -358,6 +663,14 @@ void evaluate(Evaluator *evaluator, Node *node, Scope *scope) {
     node->value.type = evaluator->type_cache->get({.kind = TypeKind::TypeId});
     node->value.has_data = true;
     node->value.data.type_value = evaluator->type_cache->get(t);
+    break;
+  }
+  case NodeKind::UnaryOperator: {
+    evaluateUnary(evaluator, node, scope);
+    break;
+  }
+  case NodeKind::Operator: {
+    evaluateBinary(evaluator, node, scope);
     break;
   }
   }
