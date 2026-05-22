7623b9e92793706b199d98b47c65a73fdd8dcee7
PixelDoted <isaac@marketplaceplus.com>
2026-05-22 10:02:22 -0500

    Evaluate builtin types Fix integer comparison

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -4,9 +4,13 @@
 #include "print.hpp"
 #include "symbol.hpp"
 #include "types.hpp"
+#include <charconv>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <string>
+#include <system_error>
 
 #define expect(ok, srcloc, msg)                                                \
   if (!(ok)) {                                                                 \
@@ -17,6 +21,53 @@
 
 // Forward Declarations
 void evaluate(Evaluator *evaluator, Node *node, Scope *scope);
+
+Type *getBuiltinType(TypeCache *type_cache, String name) {
+  std::string str((const char *)name.ptr, name.len);
+
+  Type *out_type = nullptr;
+  if (str.compare("void") == 0) {
+    out_type = type_cache->get({.kind = TypeKind::Void});
+  } else if (str.compare("bool") == 0) {
+    out_type = type_cache->get({.kind = TypeKind::Bool});
+  } else if (str.compare("usize") == 0) {
+    Type t = {.kind = TypeKind::Integer};
+    t.integer = {.is_untyped = false, .is_signed = false, .bits = -1};
+    out_type = type_cache->get(t);
+  } else if (str.compare("isize") == 0) {
+    Type t = {.kind = TypeKind::Integer};
+    t.integer = {.is_untyped = false, .is_signed = true, .bits = -1};
+    out_type = type_cache->get(t);
+  } else if (name.len >= 2 &&
+             (name[0] == 'u' || name[0] == 'i' || name[0] == 'f')) {
+    // Integer and Float
+    uint32_t bits = 0;
+    auto [ptr, ec] = std::from_chars((const char *)(name.ptr + 1),
+                                     (const char *)(name.ptr + name.len), bits);
+
+    if (ec == std::errc{}) {
+      Type t = {.kind = TypeKind::Void};
+      if (name.ptr[0] == 'u' || name.ptr[0] == 'i') {
+        t.kind = TypeKind::Integer;
+        t.integer = {
+            .is_untyped = false,
+            .is_signed = name.ptr[0] == 'i',
+            .bits = (int32_t)bits,
+        };
+      } else if (name.ptr[0] == 'f' &&
+                 (bits == 16 || bits == 32 || bits == 64 || bits == 128)) {
+        t.kind = TypeKind::Float;
+        t._float = {.is_untyped = false, .bits = bits};
+      }
+
+      if (t.kind != TypeKind::Void) {
+        out_type = type_cache->get(t);
+      }
+    }
+  }
+
+  return out_type;
+}
 
 bool compareTypes(Type *lhs, Type *rhs) {
   if (lhs->kind != rhs->kind) {
@@ -31,9 +82,16 @@ bool compareTypes(Type *lhs, Type *rhs) {
     return true;
   }
   case TypeKind::Integer: {
-    return lhs->integer.is_signed == rhs->integer.is_signed &&
-           (lhs->integer.is_untyped || rhs->integer.is_untyped ||
-            lhs->integer.bits == rhs->integer.bits);
+    bool term1 = lhs->integer.is_untyped | lhs->integer.is_signed |
+                 !rhs->integer.is_signed;
+    bool term2 = rhs->integer.is_untyped | rhs->integer.is_signed |
+                 !lhs->integer.is_signed;
+
+    bool bits_match = lhs->integer.bits == rhs->integer.bits;
+    bool untyped_or_bits =
+        lhs->integer.is_untyped || rhs->integer.is_untyped || bits_match;
+
+    return term1 && term2 && untyped_or_bits;
   }
   case TypeKind::Float: {
     return lhs->_float.is_untyped || rhs->_float.is_untyped ||
@@ -370,11 +428,18 @@ void evaluate(Evaluator *evaluator, Node *node, Scope *scope) {
     break;
   }
   case NodeKind::Name: {
-    Symbol *symbol = scope->findSymbol(&node->text, &node->location);
-    expect(symbol != nullptr, node->location,
-           "Symbol not found: \"" << node->text << '\"');
-    evaluate(evaluator, symbol->node, symbol->parent);
-    node->value = symbol->node->value;
+    Type *builtin_type = getBuiltinType(evaluator->type_cache, node->text);
+    if (builtin_type != nullptr) {
+      node->value.type = evaluator->type_cache->get({.kind = TypeKind::TypeId});
+      node->value.has_data = true;
+      node->value.data.type_value = builtin_type;
+    } else {
+      Symbol *symbol = scope->findSymbol(&node->text, &node->location);
+      expect(symbol != nullptr, node->location,
+             "Symbol not found: \"" << node->text << '\"');
+      evaluate(evaluator, symbol->node, symbol->parent);
+      node->value = symbol->node->value;
+    }
     break;
   }
   case NodeKind::Integer: {
@@ -451,11 +516,11 @@ void evaluate(Evaluator *evaluator, Node *node, Scope *scope) {
         } else {
           node->value.type = value->type;
         }
-      } else if (node->value.type != value->type) {
+      } else if (!compareTypes(node->value.type, value->type)) {
         std::cerr << node->field.initial->location
-                  << " Field initial doesn't match type.\n";
-        std::cerr << "Field Type: " << node->value.type;
-        std::cerr << "Initial Type: " << value->type;
+                  << " Field initial doesn't match type. ";
+        std::cerr << "Field Type: `" << *node->value.type << "` ";
+        std::cerr << "Initial Type: `" << *value->type << "`\n";
         node->value.type = nullptr;
         return;
       }
