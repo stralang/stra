49fe40f75689dc79ee1e5ca324ed05c8e09b8690
PixelDoted <isaac@marketplaceplus.com>
2026-05-28 09:25:40 -0500

    Implement import handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 #include "print.hpp"
 #include "tokenizer.hpp"
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 enum class EmitMode {
   Executable,
@@ -60,38 +62,101 @@ int main(int argc, const char **argv) {
     i += 1;
   }
 
-  // Tokenize
-  Tokenizer tokenizer;
-  tokenizer.path = args.path;
-  tokenizer.init();
-
-  // Parse
-  Allocator allocator;
-  ASTParser parser = ASTParser{
-      .tokenizer = tokenizer,
-      .allocator = &allocator,
+  struct SourceFile {
+    String path;
+    Node *ast;
+    Symbol *symbol;
+    ASTParser parser;
   };
-  parser.parse();
+  struct PendingFile {
+    String path;
+    Node *importer;
+  };
 
-  // Emit AST
-  if (args.mode == EmitMode::AST) {
-    std::cout << *parser.ast << "\n";
-    return 0;
+  Allocator global_allocator;
+  ArrayList<SourceFile> files;
+  ArrayList<PendingFile> pending;
+  HashMap<uint64_t, size_t> path_to_file;
+
+  files.init(&global_allocator, 8);
+  pending.init(&global_allocator, 8);
+  path_to_file.init(&global_allocator, 64);
+
+  pending.push({
+      .path = args.path,
+      .importer = nullptr,
+  });
+
+  while (pending.length > 0) {
+    PendingFile file = pending.pop();
+
+    // Tokenize
+    Tokenizer tokenizer;
+    tokenizer.path = file.path;
+    tokenizer.init();
+
+    // Parse
+    ASTParser parser = ASTParser{
+        .tokenizer = tokenizer,
+        .allocator = &global_allocator,
+    };
+    parser.parse();
+
+    // Emit AST
+    if (args.mode == EmitMode::AST) {
+      std::cout << *parser.ast << "\n";
+      return 0;
+    }
+
+    // Finish
+    files.push({
+        .path = file.path,
+        .ast = parser.ast,
+        .symbol = parser.symbol,
+        .parser = parser,
+    });
+
+    Hasher hasher;
+    hasher.hash_raw(file.path.ptr, file.path.len);
+    path_to_file.insert(hasher.state, files.length - 1);
+
+    if (file.importer != nullptr) {
+      file.importer->import.node = parser.ast;
+      file.importer->import.scope = parser.symbol;
+    }
+
+    // Handle Imports
+    for (size_t i = 0; i < parser.imports.length; i++) {
+      Node *import = parser.imports.data.ptr[i];
+      String import_path = import->import.path;
+
+      Hasher hasher;
+      hasher.hash_raw(import_path.ptr, import_path.len);
+      size_t *import_idx = path_to_file.get(hasher.state);
+      if (import_idx != nullptr) {
+        SourceFile *import_file = files.data.ptr + *import_idx;
+        import->import.node = import_file->ast;
+        import->import.scope = import_file->symbol;
+      } else {
+        pending.push({.path = import_path, .importer = import});
+      }
+    }
   }
 
   // Evaluate
+  SourceFile *root_file = files.data.ptr;
   TypeCache type_cache;
   Evaluator evaluator = {
-      .ast = parser.ast,
-      .symbol = parser.symbol,
+      .ast = root_file->ast,
+      .symbol = root_file->symbol,
       .type_cache = &type_cache,
-      .allocator = &allocator,
+      .allocator = &global_allocator,
   };
   evaluator.eval();
 
   // Emit Evaluted AST
   if (args.mode == EmitMode::EvaluatedAST) {
-    std::cout << *parser.ast << "\n";
+    std::cout << *root_file->ast << "\n";
     return 0;
   }
 
@@ -99,14 +164,37 @@ int main(int argc, const char **argv) {
   CodeGenContext codegen_ctx;
   codegen_ctx.init();
 
-  CodeGenModule codegen = {
-      .source_path = tokenizer.path,
-      .ast = parser.ast,
-      .symbol = parser.symbol,
-      .allocator = &allocator,
-  };
-  codegen.output_path = "out.ll";
-  codegen.generate(&codegen_ctx);
+  ArrayList<String> outputs;
+  outputs.init(&global_allocator, 8);
+
+  for (size_t i = 0; i < files.length; i++) {
+    SourceFile *file = files.data.ptr + i;
+
+    // Get File name
+    std::string cpp_str((const char *)file->path.ptr, file->path.len);
+    std::filesystem::path path = cpp_str;
+    std::filesystem::path name = path.filename();
+    name.replace_extension(".ll");
+
+    std::string cpp_name = name.string();
+
+    String out_name = {
+        .len = cpp_name.size(),
+        .ptr = (uint8_t *)global_allocator.alloc(cpp_name.size()),
+    };
+    memcpy(out_name.ptr, cpp_name.data(), cpp_name.size());
+    outputs.push(out_name);
+
+    // Generate
+    CodeGenModule codegen = {
+        .source_path = file->path,
+        .ast = file->ast,
+        .symbol = file->symbol,
+        .allocator = &global_allocator,
+    };
+    codegen.output_path = out_name;
+    codegen.generate(&codegen_ctx);
+  }
 
   // Emit Evaluted IR
   if (args.mode == EmitMode::IR) {
@@ -114,12 +202,30 @@ int main(int argc, const char **argv) {
   }
 
   // Link
-  if (args.mode == EmitMode::Executable) {
-    std::system("clang out.ll -o out");
-  } else if (args.mode == EmitMode::Object) {
-    std::system("clang -c out.ll -o out.o");
+  std::string clang_cmd = "clang";
+  for (size_t i = 0; i < outputs.length; i++) {
+    String output = outputs.data.ptr[i];
+    std::string cpp_output((const char *)output.ptr, output.len);
+
+    clang_cmd.append(" ");
+    clang_cmd.append(cpp_output);
   }
-  std::filesystem::remove("out.ll");
+
+  if (args.mode == EmitMode::Executable) {
+    clang_cmd.append(" -o out");
+    std::cout << clang_cmd << "\n";
+    std::system(clang_cmd.data());
+  } else if (args.mode == EmitMode::Object) {
+    clang_cmd.append(" -c -o out.o");
+    std::system(clang_cmd.data());
+  }
+
+  // Cleanup
+  for (size_t i = 0; i < outputs.length; i++) {
+    String output = outputs.data.ptr[i];
+    std::string cpp_output((const char *)output.ptr, output.len);
+    std::filesystem::remove(cpp_output);
+  }
 
   std::cout << "Compilation Success\n";
 
